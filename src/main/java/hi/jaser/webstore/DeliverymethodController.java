0486a679601e167f24b60480d75a5f1744bfc8a6
janusbirgisson <janusbirgisson@gmail.com>
2023-04-06 15:28:25 +0000

    Pick-up function finished

--- src/main/java/hi/jaser/webstore/DeliverymethodController.java
+++ src/main/java/hi/jaser/webstore/DeliverymethodController.java
@@ -1,8 +1,13 @@
 package hi.jaser.webstore;
 
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import vinnsla.Karfa;
 
 public class DeliverymethodController {
 
@@ -12,10 +17,39 @@ public class DeliverymethodController {
     @FXML
     Button fxPickUp;
 
+    private static final String nameofStore = "Jaser Electronics";
+    private static final String message1 = "Your Order Has been confirmed!";
+    private static final String message = "Your order will be ready for pick-up in 2-3 business days";
+    private static final String Okay = "Back to home page";
+
+    private PontunController pontunController;
+
+    @FXML
+    private void initialize() {
+        pontunController = (PontunController) ViewSwitcher.lookup(View.PONTUN);
+    }
+
     @FXML
     private void fxTilBakaHandler (ActionEvent e){
         ViewSwitcher.switchTo(View.GREIDSLA);
     }
+    @FXML
+    private void fxPickUpHandler(ActionEvent e) {
+        int i= pontunController.karfa.getsize();
+        pontunController.karfa.getVeitingar().remove(0, i);
 
+        ButtonType bType = new ButtonType(Okay,
+                ButtonBar.ButtonData.OK_DONE);
+        Alert a = stofnaAlert(bType);
+        a.showAndWait();
+        ViewSwitcher.switchTo(View.PONTUN);
+    }
 
+    private Alert stofnaAlert(ButtonType bILagi) {
+        // Væri hægt að segja Alert.AlertType.CONFIRMATION en þá stjórnum við ekki útliti hnappanna
+        Alert a = new Alert(Alert.AlertType.NONE, message, bILagi);
+        a.setTitle(nameofStore);
+        a.setHeaderText(message1);
+        return a;
+    }
 }
--- src/main/java/vinnsla/Matsedill.java
+++ src/main/java/vinnsla/Matsedill.java
@@ -29,22 +29,22 @@ public class Matsedill {
      * a method to set the menu items;
      */
     public void setjaGogn(){
-        veitingar.add(new Veitingar(new SimpleStringProperty("iPhone1"), new SimpleStringProperty("150000")));
-        veitingar.add(new Veitingar(new SimpleStringProperty("iPhone2"), new SimpleStringProperty("3600")));
-        veitingar.add(new Veitingar(new SimpleStringProperty("iPhone3"), new SimpleStringProperty("2500")));
-        veitingar.add(new Veitingar(new SimpleStringProperty("iPhone4"), new SimpleStringProperty("3600")));
-        veitingar.add(new Veitingar(new SimpleStringProperty("Tablet1"), new SimpleStringProperty("3600")));
-        veitingar.add(new Veitingar(new SimpleStringProperty("Tablet2"), new SimpleStringProperty("3600")));
-        veitingar.add(new Veitingar(new SimpleStringProperty("Tablet3"), new SimpleStringProperty("3600")));
-        veitingar.add(new Veitingar(new SimpleStringProperty("Tablet4"), new SimpleStringProperty("3600")));
-        veitingar.add(new Veitingar(new SimpleStringProperty("Laptop1"), new SimpleStringProperty("3600")));
-        veitingar.add(new Veitingar(new SimpleStringProperty("Laptop2"), new SimpleStringProperty("3600")));
-        veitingar.add(new Veitingar(new SimpleStringProperty("Laptop3"), new SimpleStringProperty("3600")));
-        veitingar.add(new Veitingar(new SimpleStringProperty("Laptop4"), new SimpleStringProperty("3600")));
-        veitingar.add(new Veitingar(new SimpleStringProperty("Accessory1"), new SimpleStringProperty("3600")));
-        veitingar.add(new Veitingar(new SimpleStringProperty("Accessory2"), new SimpleStringProperty("3600")));
-        veitingar.add(new Veitingar(new SimpleStringProperty("Accessory3"), new SimpleStringProperty("3600")));
-        veitingar.add(new Veitingar(new SimpleStringProperty("Accessory4"), new SimpleStringProperty("3600")));
+        veitingar.add(new Veitingar(new SimpleStringProperty("iPhone 3G"), new SimpleStringProperty("119995")));
+        veitingar.add(new Veitingar(new SimpleStringProperty("Samsung Galaxy 4"), new SimpleStringProperty("109995")));
+        veitingar.add(new Veitingar(new SimpleStringProperty("Google Pixel 6"), new SimpleStringProperty("99990")));
+        veitingar.add(new Veitingar(new SimpleStringProperty("Sony Xperia IV"), new SimpleStringProperty("89990")));
+        veitingar.add(new Veitingar(new SimpleStringProperty("iPad Air"), new SimpleStringProperty("89995")));
+        veitingar.add(new Veitingar(new SimpleStringProperty("Google Pixel Tablet"), new SimpleStringProperty("79900")));
+        veitingar.add(new Veitingar(new SimpleStringProperty("iPad Pro"), new SimpleStringProperty("105995")));
+        veitingar.add(new Veitingar(new SimpleStringProperty("Galaxy Pad S7"), new SimpleStringProperty("69900")));
+        veitingar.add(new Veitingar(new SimpleStringProperty("MacBook Pro"), new SimpleStringProperty("249900")));
+        veitingar.add(new Veitingar(new SimpleStringProperty("Samsung Galaxy Book"), new SimpleStringProperty("169000")));
+        veitingar.add(new Veitingar(new SimpleStringProperty("MacBook Air"), new SimpleStringProperty("199995")));
+        veitingar.add(new Veitingar(new SimpleStringProperty("Galaxy Book Pro"), new SimpleStringProperty("189900")));
+        veitingar.add(new Veitingar(new SimpleStringProperty("Phone Charger"), new SimpleStringProperty("7990")));
+        veitingar.add(new Veitingar(new SimpleStringProperty("Tablet Pencil"), new SimpleStringProperty("15900")));
+        veitingar.add(new Veitingar(new SimpleStringProperty("Laptop Charger"), new SimpleStringProperty("12995")));
+        veitingar.add(new Veitingar(new SimpleStringProperty("Airpods"), new SimpleStringProperty("34995")));
 
     }
 
